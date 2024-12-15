9a39b6fe96857a2826bcdd630e17ff07b3f84007
boygemini <gbadebojubril@gmail.com>
2024-12-15 16:03:33 +0100

    done

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -9,31 +9,56 @@ import Login from "./pages/Login";
 import Cards from "./pages/Cards";
 import More from "./pages/More";
 import ErrorBoundary from "./components/ErrorBoundary";
-import { useEffect } from "react";
+import { AuthProvider } from "./components/AuthContext"; // Path to your AuthContext
+import ProtectedRoute from "./components/ProtectedRoute"; // Path to ProtectedRoute
 
 function App() {
-  useEffect(() => {
-    if (window.location.href.includes("login")) {
-      console.log("logged out");
-    } else {
-      localStorage.getItem("loggedin")
-        ? console.log("logged in")
-        : (window.location.href = "/login");
-    }
-  }, []);
   return (
     <div className="body">
       <ErrorBoundary>
-        <Banner />
-        <Routes>
-          <Route path="/" element={<Accounts />} />
-          <Route path="/transfer" element={<Transfer />} />
-          <Route path="/cards" element={<Cards />} />
-          <Route path="/more" element={<More />} />
-          <Route path="*" element={<Login />} />{" "}
-          <Route path="/login" element={<Login />} />
-        </Routes>
-        <Menu />
+        <AuthProvider>
+          <Banner />
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <ProtectedRoute>
+                  <Accounts />
+                  <Menu />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/transfer"
+              element={
+                <ProtectedRoute>
+                  <Transfer />
+                  <Menu />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/cards"
+              element={
+                <ProtectedRoute>
+                  <Cards />
+                  <Menu />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/more"
+              element={
+                <ProtectedRoute>
+                  <More />
+                  <Menu />
+                </ProtectedRoute>
+              }
+            />
+            <Route path="*" element={<Login />} />{" "}
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </AuthProvider>
       </ErrorBoundary>
     </div>
   );
--- web/src/pages/More.jsx
+++ web/src/pages/More.jsx
@@ -1,16 +1,20 @@
 import Lock from "../assets/lock-2.png";
+import { useAuth } from "../components/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 const More = () => {
-  const logout = () => {
-    sessionStorage.removeItem("loggedin");
-    window.location.href = "/login";
+  const { logout } = useAuth();
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    logout();
+    navigate("/");
   };
   return (
     <div className="major-container">
       <div className="card-container">
         <img src={Lock} alt="" />
         <p>Sorry, you don't have the permission to view this time.</p>
-        <button className="logout" onClick={logout}>
+        <button className="logout" onClick={handleLogout}>
           Logout
         </button>
       </div>
