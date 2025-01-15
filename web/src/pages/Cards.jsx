c955d6b73d5e3abcbcc880783f261f0f12302041
Jubril Bolajoko Gbadebo <gbadebojubril@gmail.com>
2025-01-15 10:12:02 -0800

    Update Cards.jsx

--- web/src/pages/Cards.jsx
+++ web/src/pages/Cards.jsx
@@ -3,7 +3,7 @@ const Cards = () => {
     <div className="major-container">
       <h1 className="section-title">Cards</h1>
       <div className="card-div">
-        <p>Sorry, you have no cards, please verify your account and try again</p>
+        <p>Sorry, you have no cards.</p>
       </div>
     </div>
   );
